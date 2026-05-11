4755fc65df8c073c5280dce493e8228852aec9a5
xielongxiang623 <xielongxiang623@gmail.com>
2026-05-11 13:25:54 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '12682681-6495-4386-9bae-ced75a74b421';
 
 let proxyIP = '';
 
